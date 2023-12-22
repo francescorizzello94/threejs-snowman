0642a5c9aa76b81f73450e6749a61440756d2fc3
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2023-12-22 15:00:38 +0100

    FEAT: snowfall only upon toggle

--- script.js
+++ script.js
@@ -1,111 +1,136 @@
 function init() {
-    const scene = new THREE.Scene();
-    
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-    camera.position.set(0, 0, 50); // Move the camera back along the z-axis to see the scene
-    
-    const sizes = {
-        width: window.innerWidth,
-        height: window.innerHeight,
-    }
+  const scene = new THREE.Scene();
 
-    // Snowman's body
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100
+  );
+  camera.position.set(0, 0, 50); // Move the camera back along the z-axis to see the scene
 
-    const snowManHead = new THREE.Mesh(new THREE.SphereGeometry(5, 32, 32), new THREE.MeshBasicMaterial({ color: 0xFFFFFF }));
-    const snowmanTorso = new THREE.Mesh(new THREE.SphereGeometry(7, 32, 32), new THREE.MeshBasicMaterial({ color: 0xFFFFFF }));
-    const snowmanBase = new THREE.Mesh(new THREE.SphereGeometry(10, 32, 32), new THREE.MeshBasicMaterial({ color: 0xFFFFFF }));
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
 
-    snowManHead.position.set(0, 12, 0);
-    snowmanTorso.position.set(0, 0, 0);
-    snowmanBase.position.set(0, -15, 0);
+  // Snowman's body
 
-    scene.add(snowManHead);
-    scene.add(snowmanTorso);
-    scene.add(snowmanBase);
+  const snowManHead = new THREE.Mesh(
+    new THREE.SphereGeometry(5, 32, 32),
+    new THREE.MeshBasicMaterial({ color: 0xffffff })
+  );
+  const snowmanTorso = new THREE.Mesh(
+    new THREE.SphereGeometry(7, 32, 32),
+    new THREE.MeshBasicMaterial({ color: 0xffffff })
+  );
+  const snowmanBase = new THREE.Mesh(
+    new THREE.SphereGeometry(10, 32, 32),
+    new THREE.MeshBasicMaterial({ color: 0xffffff })
+  );
 
-    // Directional light
+  snowManHead.position.set(0, 12, 0);
+  snowmanTorso.position.set(0, 0, 0);
+  snowmanBase.position.set(0, -15, 0);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(0, 1, 0);
-    scene.add(directionalLight);
+  scene.add(snowManHead);
+  scene.add(snowmanTorso);
+  scene.add(snowmanBase);
 
-    // Ambient light
+  // Directional light
 
-    const ambientLight = new THREE.AmbientLight(0x404040);
-    scene.add(ambientLight);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(0, 1, 0);
+  scene.add(directionalLight);
 
-    // Add snow
+  // Ambient light
 
-    const snowParticleCount = 5000;
-    // each snow particle has x, y and z coordinates
-    const snowParticlesPositions = new Float32Array(snowParticleCount * 3);
+  const ambientLight = new THREE.AmbientLight(0x404040);
+  scene.add(ambientLight);
 
-    for (let i = 0; i < 1000; i++) {
-        snowParticlesPositions[i * 3 + 0] = Math.random() * 200 - 100;
-        snowParticlesPositions[i * 3 + 1] = Math.random() * 200 - 100;
-        snowParticlesPositions[i * 3 + 2] = Math.random() * 200 - 100;
-    }
+  // Create controls for the GUI
+  const controls = new (function () {
+    this.snowVisible = false;
+  })();
+  // Create the GUI
+  const gui = new dat.GUI();
+  gui
+    .add(controls, "snowVisible")
+    .name("Snowfall")
+    .onChange(function (value) {
+      snow.visible = value;
+    });
 
-    const snowGeometry = new THREE.BufferGeometry;
-    snowGeometry.setAttribute('position', new THREE.BufferAttribute(snowParticlesPositions, 3));
-    const snowMaterial = new THREE.PointsMaterial({color: 0xFFFFFF, size: 0.5});
-    
-    const snow = new THREE.Points(snowGeometry, snowMaterial);
-    scene.add(snow);
+  // Add snow
 
-    // Regulate snow speed
+  const snowParticleCount = 5000;
+  // each snow particle has x, y and z coordinates
+  const snowParticlesPositions = new Float32Array(snowParticleCount * 3);
 
-    const snowSpeed = new Float32Array(snowParticleCount);
+  for (let i = 0; i < 1000; i++) {
+    snowParticlesPositions[i * 3 + 0] = Math.random() * 200 - 100;
+    snowParticlesPositions[i * 3 + 1] = Math.random() * 200 - 100;
+    snowParticlesPositions[i * 3 + 2] = Math.random() * 200 - 100;
+  }
+
+  const snowGeometry = new THREE.BufferGeometry();
+  snowGeometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(snowParticlesPositions, 3)
+  );
+  const snowMaterial = new THREE.PointsMaterial({ color: 0xffffff, size: 0.5 });
+
+  const snow = new THREE.Points(snowGeometry, snowMaterial);
+  snow.visible = controls.snowVisible;
+  scene.add(snow);
+
+  // Regulate snow speed
+
+  const snowSpeed = new Float32Array(snowParticleCount);
+  for (let i = 0; i < snowParticleCount; i++) {
+    // snowflakes fall at random speed
+    snowSpeed[i] = 0.1 + Math.random() * 0.2;
+  }
+
+  // Snow motion effects
+
+  function addSnowMotionEffects() {
     for (let i = 0; i < snowParticleCount; i++) {
-        // snowflakes fall at random speed
-        snowSpeed[i] = 0.1 + Math.random() * 0.2;
+      snowGeometry.attributes.position.array[i * 3 + 1] -= snowSpeed[i];
+      if (snowGeometry.attributes.position.array[i * 3 + 1] < -100) {
+        // reset snowflake to the top
+        snowGeometry.attributes.position.array[i * 3 + 1] = 100;
+        // reset x position to spread snowflakes out again
+        snowGeometry.attributes.position.array[i * 3 + 0] =
+          Math.random() * 200 - 100;
+      }
+
+      // add sway based on snowflake's y poistion
+      snowGeometry.attributes.position.array[i * 3 + 0] +=
+        Math.sin(
+          Date.now() * 0.001 + snowGeometry.attributes.position.array[i * 3 + 1]
+        ) * 0.01;
     }
 
-    // Snow motion effects
+    // update the snowflakes' position
+    snowGeometry.attributes.position.needsUpdate = true;
+  }
 
-    function addSnowMotionEffects() {
-        for (let i = 0;  i < snowParticleCount; i++) {
-            snowGeometry.attributes.position.array[i * 3 + 1] -= snowSpeed[i];
-            if (snowGeometry.attributes.position.array[i * 3 + 1] < -100) {
-                // reset snowflake to the top
-                snowGeometry.attributes.position.array[i * 3 + 1] = 100;
-                // reset x position to spread snowflakes out again
-                snowGeometry.attributes.position.array[i * 3 + 0] = Math.random() * 200 - 100;
-            }
+  // Set up webgl renderer
 
-            // add sway based on snowflake's y poistion
-            snowGeometry.attributes.position.array[i * 3 + 0] += Math.sin(Date.now() * 0.001 + snowGeometry.attributes.position.array[i * 3 + 1]) * 0.01;
-        }
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(sizes.width, sizes.height);
 
-            // update the snowflakes' position
-        snowGeometry.attributes.position.needsUpdate = true;
-    }
+  // Add the output of the render function to the HTML
+  document.body.appendChild(renderer.domElement);
 
-    // Set up webgl renderer
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(sizes.width, sizes.height);
-
-    // Add the output of the render function to the HTML
-    document.body.appendChild(renderer.domElement);
-
-    // Create controls for the GUI
-    const controls = new function () {
-        this.toggleSnowFall = function() {
-            snow.visible = !snow.visible;
-        }
-    };
-    // Create the GUI
-    const gui = new dat.GUI();
-    gui.add(controls, 'toggleSnowFall').name('Toggle snowfall');
-
-    // Function for re-rendering/animating the scene
-    function tick() {
-        addSnowMotionEffects();
-        requestAnimationFrame(tick);
-        renderer.render(scene, camera);
-    }
-    tick();
+  // Function for re-rendering/animating the scene
+  function tick() {
+    addSnowMotionEffects();
+    requestAnimationFrame(tick);
+    renderer.render(scene, camera);
+  }
+  tick();
 }
 
 init();
